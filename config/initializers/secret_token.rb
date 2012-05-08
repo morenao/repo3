4abac9047956462d33e35f43297f35f2f9c7a54f
Morenao <amoreno@live.com>
2012-05-08 17:33:42 -0300

    Primeiro commit rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repo3::Application.config.secret_token = 'b05ab42a94d83c6c4f7eb67296ff81477502b14d7fee2b80a9c975df47d3f542d661ac5f8ae8dd91cb6f9bcd2a5e6e07b0466e5096836ccb7c9adab4bb73193a'
